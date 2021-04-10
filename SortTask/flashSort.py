c260ef462680e2050d4004d520504f4a17d28090
Andrew Razumov <and.razumov2014@gmail.com>
2021-04-11 05:01:40 +0800

    strand sort and flashsort

---
+++ SortTask/flashSort.py
@@ -0,0 +1,47 @@
+def flash_sort(input_array):
+    array_length = len(input_array)
+    m = int(array_length * 0.42)
+    sublist = [0] * m
+    i, j, k = 0, 0, 0
+    min_elem = input_array[0]
+    max_elem_index = 0
+    for i in range(1, array_length, 1):
+        if input_array[i] < min_elem:
+            min_elem = input_array[i]
+        if input_array[i] > input_array[max_elem_index]:
+            max_elem_index = i
+    if min_elem == input_array[max_elem_index]:
+        return
+    c1 = (m - 1) / (input_array[max_elem_index] - min_elem)
+    for i in range(array_length):
+        k = int(c1 * (input_array[i] - min_elem))
+        sublist[k] += 1
+    for i in range(1, m, 1):
+        sublist[i] += sublist[i - 1]
+    input_array[max_elem_index], input_array[0] = input_array[0], input_array[max_elem_index]
+    elems_moved = 0
+    flash = None
+    j = 0
+    k = m - 1
+    while elems_moved < array_length - 1:
+        while j > (sublist[k] - 1):
+            j += 1
+            k = int(c1 * (input_array[j] - min_elem))
+        flash = input_array[j]
+        while not j == sublist[k]:
+            k = int(c1 * (flash - min_elem))
+            input_array[sublist[k] - 1], flash = flash, input_array[sublist[k] - 1]
+            sublist[k] -= 1
+            elems_moved += 1
+    for i in range(len(input_array) - 3, -1, -1):
+        if input_array[i + 1] < input_array[i]:
+            buffer = input_array[i]
+            j = i
+            while input_array[j + 1] < buffer:
+                input_array[j] = input_array[j + 1]
+                j += 1
+            input_array[j] = buffer
+    return input_array
+
+
+print(flash_sort([10, 29, 18, 2, 4, 91, 105, 0, 0, 101, -10]))
---
+++ SortTask/strandSort.py
@@ -0,0 +1,53 @@
+def strand_sort(input_arr, descending_order=False):
+    output_arr = []
+    while len(input_arr):
+        indexes_to_pop = []
+        sublist = [input_arr[0]]
+        indexes_to_pop.append(0)
+        sublist_index = 0
+        for i in range(1, len(input_arr), 1):
+            if descending_order:
+                if input_arr[i] < sublist[sublist_index]:
+                    sublist.append(input_arr[i])
+                    sublist_index += 1
+                    indexes_to_pop.append(i)
+            else:
+                if input_arr[i] > sublist[sublist_index]:
+                    sublist.append(input_arr[i])
+                    sublist_index += 1
+                    indexes_to_pop.append(i)
+        for i in range(len(indexes_to_pop) - 1, -1, -1):
+            input_arr.pop(indexes_to_pop[i])
+        output_arr = merge(sublist, output_arr, descending_order)
+    return output_arr
+
+
+def merge(arr1, arr2, descending_order):
+    united_array = []
+    array1_index = 0
+    array2_index = 0
+    while True:
+        if array1_index == len(arr1):
+            united_array += arr2[array2_index:len(arr2)]
+            break
+        if array2_index == len(arr2):
+            united_array += arr1[array1_index:len(arr1)]
+            break
+        if descending_order:
+            if arr1[array1_index] >= arr2[array2_index]:
+                united_array.append(arr1[array1_index])
+                array1_index += 1
+            else:
+                united_array.append(arr2[array2_index])
+                array2_index += 1
+        else:
+            if arr1[array1_index] <= arr2[array2_index]:
+                united_array.append(arr1[array1_index])
+                array1_index += 1
+            else:
+                united_array.append(arr2[array2_index])
+                array2_index += 1
+    return united_array
+
+
+print(strand_sort([10, 29, 18, 2, 4, 91, 105, 0, 0, 101, -10], False))
