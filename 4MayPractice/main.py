5c3b34c43f5e471e497c8221757120d97d77397c
Andrew Razumov <and.razumov2014@gmail.com>
2021-06-03 22:28:03 +0800

    '???'

--- 4MayPractice/main.py
+++ 4MayPractice/main.py
@@ -3,6 +3,7 @@ import pytz
 import matplotlib.pyplot as plt
 from datetime import datetime, timezone
 from some_optimization import DateLoader
+import pickle
 
 
 if __name__ == '__main__':
@@ -18,7 +19,7 @@ if __name__ == '__main__':
     else:
         epoch = datetime(2020, 5, 20, 12, 30, 0, tzinfo=timezone.utc)
         before = time.time()
-        data = date_loader.get_map(epoch, 'dtec_20_60')
+        data = date_loader.get_map_modified(epoch, 'dtec_20_60')
         print(f'It took {time.time() - before} sec. to retrieve a map')
         val = data[:, 0]
         x = data[:, 1]
--- 4MayPractice/some_optimization.py
+++ 4MayPractice/some_optimization.py
@@ -1,25 +1,31 @@
 import h5py
 import numpy as np
+import os
+import pickle
 
 
 class DateLoader:
     file = None
+    __written = None
 
     def __init__(self, pth):
         self.file = h5py.File(pth, 'r')
 
+    # can be problematic
     def __get_sites(self):
         sites = []
         for site in self.file:
             sites.append(site)
         return sites
 
+    # can be problematic
     def __get_sats(self, site, fhdf=None):
         file = self.file if not fhdf else fhdf
         if site in file:
             sats = [sat for sat in file[site]]
         return sats
 
+    # can be problematic
     def __get_data(self, site, sat, field, fhdf=None):
         file = self.file if not fhdf else fhdf
         if site in file and sat in file[site]:
@@ -31,23 +37,92 @@ class DateLoader:
         data = self.__get_data(site, sat, field)
         return ts, data
 
+#
+    def __save_data(self, path, site, field):
+        data_dictionary = {
+            'lat': None,
+            'lon': None,
+            'sats_data': None
+        }
+        with open(path, 'wb') as f:
+            lat = self.file[site].attrs['lat']
+            lon = self.file[site].attrs['lon']
+            pickle.dump(lat, f)
+            pickle.dump(lon, f)
+            sats = [sat for sat in self.file[site]]
+            sats_data = []
+            for sat in sats:
+                timestamps, data = self.get_series(site, sat, field)
+                sats_data.append((timestamps, data))
+            pickle.dump(sats_data, f)
+            data_dictionary.update({'lat': lat})
+            data_dictionary.update({'lon': lon})
+            data_dictionary.update({'sats_data': sats_data})
+        return data_dictionary
+
+    def __read_data(self, path):
+        data_dictionary = {
+            'lat': None,
+            'lon': None,
+            'sats_data': None
+        }
+        with open(path, 'rb') as f:
+            data_dictionary.update({'lat': pickle.load(f)})
+            data_dictionary.update({'lon': pickle.load(f)})
+            data_dictionary.update({'sats_data': pickle.load(f)})
+        return data_dictionary
+#
+
     def get_map(self, time, field):
         result = []
         timestamp = time.timestamp()
         start = timestamp
         end = timestamp
-        sites = self.__get_sites()
+        sites = self.file.keys()
         for site in sites:
             lat = np.degrees(self.file[site].attrs['lat'])
             lon = np.degrees(self.file[site].attrs['lon'])
+            # it can be trouble
             sats = self.__get_sats(site, fhdf=self.file)
+            print(site, end=', amount of satellites: ')
+            print(len(sats))
+            #
             for sat in sats:
-                timestamps = self.__get_data(site, sat, 'timestamp', fhdf=self.file)
-                data = self.__get_data(site, sat, field, fhdf=self.file)
+                # it can be trouble
+                timestamps, data = self.get_series(site, sat, field)
+                #
                 match = np.where((timestamps >= start) & (timestamps <= end))
                 data_match = data[match]
                 for d in data_match:
                     result.append((d, lon, lat))
+        if not result:
+            return None
+        else:
+            return np.array(result)
+
+    def get_map_modified(self, time, field):
+        result = []
+        timestamp = time.timestamp()
+        start = timestamp
+        end = timestamp
+        sites = self.file.keys()
+        if os.path.exists(f'./res/{field}'):
+            self.__written = [f for f in os.listdir(f'./res/{field}') if os.path.isfile(os.path.join(f'./res/{field}', f))]
+        else:
+            os.mkdir(f'./res/{field}')
+            self.__written = []
+        for site in sites:
+            data_dictionary = None
+            if not f'./res/{field}/{site}.dat' in self.__written:
+                data_dictionary = self.__save_data(f'./res/{field}/{site}.dat', site, field)
+            else:
+                data_dictionary = self.__read_data(f'./res/{field}/{site}.dat')
+            sats_data = data_dictionary.get('sats_data')
+            for sat_data in sats_data:
+                match = np.where((sat_data[0] >= start) & (sat_data[0] <= end))
+                data_match = sat_data[1][match]
+                for d in data_match:
+                    result.append((d, data_dictionary.get('lon'), data_dictionary.get('lat')))
         if not result:
             return None
         else:
